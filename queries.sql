8799ffbfcd4545039337e35fdfbb78148e71408c
Shaun Coulter <shaundcoulter@gmail.com>
2021-04-05 16:06:08 -0400

    emp_list.csv - Employee List made

--- queries.sql
+++ queries.sql
@@ -121,3 +121,12 @@ FROM current_emp AS ce
 LEFT JOIN dept_emp AS de
 ON ce.emp_no = de.emp_no
 GROUP BY de.dept_no;
+
+-- Employee count by department number, save table, sort and group.
+SELECT COUNT(ce.emp_no), de.dept_no
+INTO retire_dept
+FROM current_emp as ce
+LEFT JOIN dept_emp as de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no
+ORDER BY de.dept_no;
