0e2753783c185823dffafbe68157ff6b7dfe6c10
Shaun Coulter <shaundcoulter@gmail.com>
2021-04-05 15:26:14 -0400

    created more queries

--- queries.sql
+++ queries.sql
@@ -78,11 +78,46 @@ WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
   AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
 
 -- Add more specific hiring conditions and save the Table.
-SELECT first_name, last_name
+SELECT emp_no, first_name, last_name
 INTO retirement_info
 FROM employees
 WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
   AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
-
--- Query the newly made table.
+-- Test table contains correct columns.
 SELECT * FROM retirement_info;
+
+-- Joining departments and dept_manager tables
+SELECT d.dept_name,
+     dm.emp_no,
+     dm.from_date,
+     dm.to_date
+FROM departments AS d
+INNER JOIN dept_manager AS dm
+ON d.dept_no = dm.dept_no;
+
+-- Joining retirement_info and dept_emp.
+SELECT ri.emp_no,
+		ri.first_name,
+		ri.last_name,
+		de.to_date
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+
+-- Joining retirement_info and dept_emp and saving the table, with a condition.
+SELECT ri.emp_no,
+		ri.first_name,
+        ri.last_name,
+        de.to_date
+INTO current_emp
+FROM retirement_info AS ri
+LEFT JOIN dept_emp AS de
+ON ri.emp_no = de.emp_no
+WHERE de.to_date = ('9999-01-01');
+
+-- Employee count by department number.
+SELECT COUNT(ce.emp_no), de.dept_no
+FROM current_emp AS ce
+LEFT JOIN dept_emp AS de
+ON ce.emp_no = de.emp_no
+GROUP BY de.dept_no;
